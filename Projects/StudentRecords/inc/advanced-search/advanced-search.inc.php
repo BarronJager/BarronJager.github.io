a79dd258e2410c5f6d13d20c8d045b9525b13740
BarronJager <b.jager@students.clark.edu>
2024-02-16 15:24:18 -0800

    Version - 2

---
+++ Projects/StudentRecords/inc/advanced-search/advanced-search.inc.php
@@ -0,0 +1,8 @@
+<?php // Filename: advanced-search.inc.php
+
+require_once __DIR__ . "/../db/db_connect.inc.php";
+require_once __DIR__ . "/../app/config.inc.php";
+
+$degree_program = null;
+$financial_aid_yes = false;
+$financial_aid_no = false;
